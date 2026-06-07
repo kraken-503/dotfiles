081638d716c451e090a89e1f5d678fcbcf707086
kraken-503 <kraken-503@example.com>
2026-06-07 16:16:48 +0530

    modify gaps, touched up gitignore

--- hypr/.config/hypr/modules/looks.lua
+++ hypr/.config/hypr/modules/looks.lua
@@ -5,7 +5,7 @@
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 2,
+        gaps_in  = 3,
         gaps_out = 4,
 
         border_size = 0,

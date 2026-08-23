138716fbcaf3d7e5ea8caaf3aca6bf91da4443c2
kraken-503 <krakenzz503@gmail.com>
2026-08-23 23:52:53 +0530

    add: swayosd-server

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -11,5 +11,6 @@
 hl.on("hyprland.start", function ()
   hl.exec_cmd("nm-applet")
   hl.exec_cmd("waybar & awww-daemon & matuwall --daemon ")
+  hl.exec_cmd("swayosd-server")
   hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 end)

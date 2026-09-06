e2ead3a0b44bb986f061f6f2bdacf90d325a53ac
kraken-503 <krakenzz503@gmail.com>
2026-09-06 13:18:55 +0530

    add: qt6 env var

--- hypr/.config/hypr/modules/env.lua
+++ hypr/.config/hypr/modules/env.lua
@@ -23,10 +23,11 @@ hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 
 
 --Qt Variables 
+hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
-hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
 
 --Nvidia
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
